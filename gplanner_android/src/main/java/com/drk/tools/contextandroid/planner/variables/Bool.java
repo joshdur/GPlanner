88eed5b31eac799876cf58c2749c7d7d4cd24ee3
joshdur <jddjose@gmail.com>
2017-06-08 21:14:16 +0200

    (In progress) [gplanner] - remove unnecessary packages

--- gplanner_android/src/main/java/com/drk/tools/contextandroid/planner/variables/Bool.java
+++
@@ -1,5 +0,0 @@
-package com.drk.tools.contextandroid.planner.variables;
-
-public enum Bool {
-    TRUE, FALSE
-}
--- gplanner_android/src/test/java/com/drk/tools/contextandroid/MyMocks.java
+++
@@ -1,5 +0,0 @@
-package com.drk.tools.contextandroid;
-
-public enum MyMocks {
-    TEST1
-}
